f40c546ac3c8976b4451e147283042d996966955
AsyaGiff <nastia.kotsyba@gmail.com>
2025-10-26 23:34:47 +0100

    clean code

--- src/components/SavedItems.js
+++ src/components/SavedItems.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-// Компонент для кнопки збереження
 const SavedItems = ({ isSaved, onClick }) => {
   return (
     <button onClick={onClick} className="save-btn">
       <img
-        src={isSaved ? "/SavedFilled.svg" : "/Saved.svg"} // Використовуємо різні іконки для збереженого/незбереженого стану
+        src={isSaved ? "/SavedFilled.svg" : "/Saved.svg"} 
         alt="savedFunc"
         className="savedFunc"
       />
