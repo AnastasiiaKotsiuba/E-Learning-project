c0956a868bfa5de66613b1000f59736b5367184c
AsyaGiff <nastia.kotsyba@gmail.com>
2026-05-31 19:58:04 +0300

    Update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeMc_WQx6iBQZzFgx3TW-Bllvrz5x5ulk",
@@ -17,6 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-const analytics = getAnalytics(app);
 
